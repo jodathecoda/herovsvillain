cb528b1bb879a0c4d5e637d8dfd282685947079d
Joda TheCoda <jodathecoda@yahoo.com>
2021-11-15 15:32:31 +0200

    split chapters

--- splitter.py
+++ splitter.py
@@ -1,11 +1,18 @@
+import os
+
+global cwd
+cwd = os.getcwd()
+
+counter =1
+
 lines_per_file = 5000
 smallfile = None
-with open('sitandgo.txt') as bigfile:
+with open(cwd + '\\sitandgos.txt') as bigfile:
     for lineno, line in enumerate(bigfile):
         if lineno % lines_per_file == 0:
             if smallfile:
                 smallfile.close()
-            small_filename = 'chapter{}.txt'.format(lineno + lines_per_file)
+            small_filename = 'chapter{}.txt'.format(lineno)
             smallfile = open(small_filename, "w")
         smallfile.write(line)
     if smallfile:
